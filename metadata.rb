822b571e490c61d0057220df6f854b1b3a3e1e09
tflybot <cookbooks@fitsnips.net>
2016-05-02 19:01:21 -0700

    blank line?

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,5 @@ license 'all_rights'
 description 'Installs/Configures travis-cookbook-test'
 long_description 'Installs/Configures travis-cookbook-test'
 version '0.1.0'
-
-
 source_url 'https://github.com/jassinpain/travis-cookbook-test' if respond_to?(:source_url)
 issues_url 'https://github.com/jassinpain/travis-cookbook-test/issues' if respond_to?(:issues_url)
